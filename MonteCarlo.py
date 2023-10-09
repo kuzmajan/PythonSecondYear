77a233b615bc8984810a56d8502ce8bb40a79515
Jan Kuźma <jk450241@students.mimuw.edu.pl>
2023-10-09 19:14:02 +0200

    Wyznaczanie pi metodą Monte Carlo

---
+++ MonteCarlo.py
@@ -0,0 +1,17 @@
+import random
+import math
+
+
+def dlugosc(x, y):
+    return math.sqrt(x**2 + y**2)
+
+
+if __name__ == '__main__':
+    licznik = 0
+    n = 100000
+    for i in range(n):
+        a = random.random() * 2 - 1
+        b = random.random() * 2 - 1
+        if dlugosc(a, b) <= 1:
+            licznik += 1
+    print(4 * licznik/n)
